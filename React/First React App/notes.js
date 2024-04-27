d9d29b355f4d4d7ebccb3a8cdc955c14dca596cf
RosslerGKBoquiren <142700435+RosslerGKBoquiren@users.noreply.github.com>
2024-04-27 15:23:03 -0400

    Add files via upload

---
+++ React/First React App/notes.js
@@ -0,0 +1,8 @@
+//  First Reaction
+📁 public
+	📄 index.html: HTML with the root of the React app (i.e., 
+	<div id="root"></div>).
+📄 App.js: Our React code, written in a cool JavaScript syntax called JSX.
+📄 index.js: The place where our React app is started from.
+📜 package.json: A list of Node packages that help this app run, including React.
+📄 styles.css: Base styles for the React app.
